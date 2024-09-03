bb9eee0a0004abe7fc025233e6cc8ddada1d6b77
C-Lyngbjerg <christoffer.bjerge@visma.com>
2024-09-03 13:26:31 +0200

    added some QoL options

--- lua/options.lua
+++ lua/options.lua
@@ -3,10 +3,13 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+
 o.conceallevel = 1
 o.number = true
 o.cursorline = true
 o.smartindent = true
 o.tabstop = 2
 o.shiftwidth = 2
+o.scrolloff = 16
+o.relativenumber = true
+o.cursorlineopt = 'both'
